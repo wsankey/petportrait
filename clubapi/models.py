a9e2144c3b336743c97e152aa7bb0008280f33da
William Sankey <WSankey@gmail.com>
2015-05-24 12:44:32 -0400

    Updated pet model.

--- clubapi/models.py
+++ clubapi/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+
+	name = models.CharField(max_length=32, unique=True)
+	description = models.CharField(max_length=200)
+	owner = models.ForeignKey(User)
+	has_portrait = models.BooleanField(default=False)
+
+	def __unicode__(self):
+		return u'%s whose parent is %s %s' % (self.name, self.owner.first_name, self.owner.last_name)
