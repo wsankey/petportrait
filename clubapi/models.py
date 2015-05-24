c4430f7d7e140f557f81976a2db072935f9fd1dd
William Sankey <WSankey@gmail.com>
2015-05-24 12:49:22 -0400

    Added the Portrait model.

--- clubapi/models.py
+++ clubapi/models.py
@@ -10,3 +10,14 @@ class Pet(models.Model):
 
 	def __unicode__(self):
 		return u'%s whose parent is %s %s' % (self.name, self.owner.first_name, self.owner.last_name)
+
+class Portrait(models.Model):
+
+	style = models.CharField(max_length=32, unique=True)
+	description = models.CharField(max_length=200)
+	sale_price = models.DecimalField(decimal_places=2, max_digits=5)
+	available = mdoels.BooleanField(default=False)
+	artist = models.ForeignKey(Artist)
+
+	def __unicode__(self):
+		return u'%s %s offers %s at %s' % (self.artist.first_name, self.artist.last_name, self.style, self.sale_price)
